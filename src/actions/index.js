9e6c02ed04cf09c087c92d2daa29e626d3e07c19
Bibi Khadiza <limakhadiza@yahoo.com>
2017-01-11 13:21:55 -0500

    done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,29 @@
 // define your actions here!
+
+export function addMovie(movie){
+  return{
+    type: 'ADD_MOVIE',
+    movie: movie
+  }
+}
+
+export function deleteMovie(movie){
+  return{
+  type: 'DELETE_MOVIE',
+  movie: movie
+  }
+}
+
+export function updateUsername(user){
+  return{
+    type: 'UPDATE_USERNAME',
+    username: user
+  }
+}
+
+export function resetUsername(){
+  return{
+    type: 'RESET_USERNAME',
+    username: 'default'
+  }
+}
